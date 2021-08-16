bed491135b2ffdcc32d839ea293b1d1f36aa400d
tateejt <tateejt@gmail.com>
2021-08-15 22:05:51 -0700

    website-designed

---
+++ index.js
@@ -0,0 +1,236 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Emery Tate | Web Developer</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" crossorigin="anonymous">
+  </head>
+
+  <body>
+
+    <!-- NavBar -->
+    <div class="nav-container">
+      <header>
+          <a href="main.html" class="logo">&ltemerytate&gt</a>
+
+        <nav class="nav">
+          <ul class="nav_list">
+            <li><a href="#skills">My Skills</a></li>
+            <li><a href="#work">My Work</a></li>
+            <li><a href="#contact">Contact Me</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div class="social-header">
+        <ul>
+          <li><a href="#"><img src="/img/github-brands.svg" alt="github"></a></li>
+          <li><a href="#"><img src="/img/linkedin-brands.svg" alt="linkedin"></a></li>
+          <li><a href="#"><img src="/img/dribbble-brands.svg" alt="dribbble"></a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="intro">
+      <div class="content">
+        <h1 class="stagger">Creative Web Developer</h1>
+        <div class="meet">
+          <p>Meet Emery Tate</p>
+        </div>
+        <div class="downArrow bounce stagger">
+          <svg width="40" height="77" viewBox="0 0 38 82" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.2322 80.7678C18.2085 81.7441 19.7915 81.7441 20.7678 80.7678L36.6777 64.8579C37.654 63.8816 37.654 62.2986 36.6777 61.3223C35.7014 60.346 34.1184 60.346 33.1421 61.3223L19 75.4645L4.85786 61.3223C3.88155 60.346 2.29864 60.346 1.32233 61.3223C0.34602 62.2986 0.34602 63.8816 1.32233 64.8579L17.2322 80.7678ZM16.5 -4.63042e-10L16.5 79L21.5 79L21.5 4.63042e-10L16.5 -4.63042e-10Z" fill="white"/>
+          </svg>
+        </div>
+      </div>
+
+      <svg width="260" class="intro-design" height="592" viewBox="0 0 260 592" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path class="triangle-anim" d="M130 0L242.583 195H17.4167L130 0Z" fill="#A143FF"/>
+        <path class="triangle-anim" d="M130 130L242.583 325H17.4167L130 130Z" fill="#282828"/>
+        <path class="triangle-anim" d="M130 260L242.583 455H17.4167L130 260Z" fill="#A143FF"/>
+        <path class="triangle-anim" d="M129.583 397L242.167 592H17L129.583 397Z" fill="#282828"/>
+      </svg>
+
+    </div>
+
+    <!-- Featured Section -->
+    <section class="featured">
+        <div class="left">
+            <div class="inner transition2">
+                <p class="subtitle">Featured Project</p>
+                <a href="#" class="featured-title">Place Holder</a>
+
+                <p class="featured-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque molestiae, soluta autem nemo quae delectus quas? Excepturi.</p>
+            </div>
+        </div>
+        <img class="website right transition2" src="img/placeholder.png" alt="Featured Project">
+    </section>
+
+    <!-- My Skills -->
+    <section class="skills" id="skills">
+            <div class="skills-container">
+                <ul>
+                    <li class="transition2">
+                        <div class="icon-container one">
+                            <!-- <img src="img/placeholder.png" alt="UI/UX Icon"> -->
+                        </div>
+                        <p class="skill-title">UI UX Design</p>
+                        <p class="featured-desc skill-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, odit?</p>
+                    </li>
+                    <li class="transition2">
+                        <div class="icon-container two">
+                            <!-- <img src="img/placeholder.png" alt="UI/UX Icon"> -->
+                        </div>
+                        <p class="skill-title">Frontend Development</p>
+                        <p class="featured-desc skill-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, odit?</p>
+                    </li>
+                    <li class="transition2">
+                        <div class="icon-container three">
+                            <!-- <img src="img/placeholder.png" alt="UI/UX Icon"> -->
+                        </div>
+                        <p class="skill-title">Identity Design</p>
+                        <p class="featured-desc skill-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, odit?</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+    <!-- My Work -->
+    <section class="portfolio" id="work">
+
+        <div class="portfolio-container transition3">
+            <div class="portfolio-left">
+                <div class="inner">
+                    <p class="subtitle">Guitar App</p>
+
+                    <p class="featured-title">Fretastic.com</p>
+                    <p class="featured-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sint, molestias qui nihil accusamus praesentium vel tenetur quisquam quas rem.</p>
+                </div>
+            </div>
+            <a href="#"><img class="website" src="img/placeholder.png" alt="Fretastic"></a>
+        </div>
+
+        <div class="portfolio-container transition3">
+            <div class="portfolio-left">
+                <div class="inner">
+                    <p class="subtitle">Guitar App</p>
+
+                    <p class="featured-title">Fretastic.com</p>
+                    <p class="featured-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sint, molestias qui nihil accusamus praesentium vel tenetur quisquam quas rem.</p>
+                </div>
+            </div>
+            <a href="#"><img class="website" src="img/placeholder.png" alt="Fretastic"></a>
+        </div>
+
+        <div class="portfolio-container transition3">
+            <div class="portfolio-left">
+                <div class="inner">
+                    <p class="subtitle">Guitar App</p>
+
+                    <p class="featured-title">Fretastic.com</p>
+                    <p class="featured-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sint, molestias qui nihil accusamus praesentium vel tenetur quisquam quas rem.</p>
+                </div>
+            </div>
+            <a href="#"><img class="website" src="img/placeholder.png" alt="Fretastic"></a>
+        </div>
+    </section>
+
+<!-- Contact Form -->
+    <section class="contact" id="contact">
+      <p class="contact-title">Contact Me</p>
+      <div class="contact-container transition4">
+        <div class="contact-form">
+
+          <form action="mail.php" method="POST">
+            <input class="name-input" type="text" placeholder="Name" name="Name" required>
+            <input class="email-input" type="text" placeholder="Email" name="Email" required>
+            <input class="subject-input" type="text" placeholder="Subject" name="Subject" required>
+            <textarea class="message-input" name="Message"  placeholder="Message" rows="6" maxlength="3000"></textarea>
+            <input class="btn-submit" type="submit" value="Submit"><input type="reset" value="Clear">
+          </form>
+
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <section class="footer">
+      <div class="copyright">
+        <p>&copy Designed and Developed by Emery Tate</p>
+      </div>
+    </section>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js"></script>
+    <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/ScrollTrigger.min.js"></script>
+
+    <script>
+      gsap.registerPlugin(ScrollTrigger);
+      var t1 = gsap.timeline();
+
+      t1.from('.content', {
+        y: '-30%',
+        opacity: 0,
+        duration: 2,
+        ease: Power4.easeOut
+      })
+
+      t1.from('.stagger', {
+        opacity: 0,
+        y: -50,
+        stagger: .3,
+        ease: Power4.easeOut,
+        duration: 2
+      }, "-=1.5")
+
+      t1.from('.intro-design', {
+        opacity: 0,
+        y: 50,
+        ease: Power4.easeOut,
+        duration: 1
+      }, "-=2")
+
+      t1.from('.triangle-anim', {
+        stagger: .2,
+        scale: 0.1,
+        duration: 1,
+        ease: Back.easeOut.config(1.7)
+      })
+
+      gsap.from(".transition2", {
+        scrollTrigger: {
+            trigger: '.transition2',
+            start: "top bottom"
+        },
+        y: 50,
+        opacity: 0,
+        duration: 1.2,
+        stagger: .3
+        })
+
+        gsap.from(".transition3", {
+            scrollTrigger: {
+                trigger: ".transition3",
+                start: "top center"
+            },
+            y: 50,
+            opacity: 0,
+            duration: 1.2,
+            stagger: .6
+        })
+        gsap.from(".transition4", {
+          scrollTrigger: {
+              trigger: '.transition4',
+              start: "top center"
+          },
+          y: 50,
+          opacity: 0,
+          duration: 1.2,
+          stagger: .3
+          });
+    </script>
+  </body>
+
+</html>
